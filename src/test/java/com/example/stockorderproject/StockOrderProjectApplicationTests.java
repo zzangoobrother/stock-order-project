34b89f434a0d9268a4cd6429b75795a4e36bc138
choiseonkang <com1368@naver.com>
2025-03-05 20:58:51 +0900

    first commit

---
+++ src/test/java/com/example/stockorderproject/StockOrderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockorderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
