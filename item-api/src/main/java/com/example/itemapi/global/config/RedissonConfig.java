42efec5a2aacd43428d8979b6aaa72f069baae13
choiseonkang <com1368@naver.com>
2025-03-23 14:16:09 +0900

    feat : Redis Cluster docker compose 작성 및 Redis config 수정

--- item-api/src/main/java/com/example/itemapi/global/config/RedissonConfig.java
+++ item-api/src/main/java/com/example/itemapi/global/config/RedissonConfig.java
@@ -1,10 +1,10 @@
 package com.example.itemapi.global.config;
 
+import com.example.itemapi.global.config.properties.RedisClusterProperties;
 import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +14,22 @@ public class RedissonConfig {
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
-    private final RedisProperties redisProperties;
+    private final RedisClusterProperties redisClusterProperties;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisProperties.getHost() + ":" + redisProperties.getPort());
+        config.useClusterServers()
+                .addNodeAddress(
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host1() + ":" + redisClusterProperties.port1(),
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host2() + ":" + redisClusterProperties.port2(),
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host3() + ":" + redisClusterProperties.port3(),
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host4() + ":" + redisClusterProperties.port4(),
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host5() + ":" + redisClusterProperties.port5(),
+                        REDISSON_HOST_PREFIX + redisClusterProperties.host6() + ":" + redisClusterProperties.port6()
+                )
+                .setScanInterval(2000);
+
         return Redisson.create(config);
     }
 }
