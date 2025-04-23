93c0e939bc97ccc3e223932d80bb51c4331068cc
choiseonkang <com1368@naver.com>
2025-04-23 20:22:19 +0900

    feat : 제품 조회 / 주문하기 대기열 토큰 검증 실패시 redirect uri 반환 추가

--- item-api/src/main/java/com/example/itemapi/interfaces/presentation/ItemController.java
+++ item-api/src/main/java/com/example/itemapi/interfaces/presentation/ItemController.java
@@ -5,10 +5,16 @@ import com.example.itemapi.interfaces.presentation.feign.QueueClient;
 import com.example.itemapi.interfaces.presentation.request.ItemRequest;
 import com.example.itemapi.interfaces.presentation.response.ItemInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Slf4j
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
@@ -26,7 +32,19 @@ public class ItemController {
 
     @GetMapping("/items/{itemId}")
     public ResponseEntity<ItemInfoResponse> getBy(@PathVariable(name = "itemId") Long itemId) {
-        validateQueueProceed();
+        boolean result = queueClient.getBy("order");
+        if (!result) {
+            try {
+                URI redirectUri = new URI("http://localhost:8083/api/v1/queue");
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setLocation(redirectUri);
+                return ResponseEntity.status(HttpStatus.SEE_OTHER).headers(httpHeaders).build();
+            } catch (URISyntaxException e) {
+                log.error("url 변환 오류");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        }
+
         ItemInfoResponse itemInfoResponse = ItemInfoResponse.toItemInfoResponse(itemService.getBy(itemId));
         return ResponseEntity.ok().body(itemInfoResponse);
     }
@@ -35,12 +53,5 @@ public class ItemController {
     public ResponseEntity<Void> deleteBy(@PathVariable Long itemId) {
         itemService.deleteBy(itemId);
         return ResponseEntity.noContent().build();
-    }
-
-    private void validateQueueProceed() {
-        boolean result = queueClient.getBy("order");
-        if (!result) {
-            throw new IllegalArgumentException("토큰 확인 필요");
-        }
     }
 }
--- order-api/src/main/java/com/example/orderapi/interfaces/presentation/OrderController.java
+++ order-api/src/main/java/com/example/orderapi/interfaces/presentation/OrderController.java
@@ -4,9 +4,16 @@ import com.example.orderapi.application.service.OrderService;
 import com.example.orderapi.interfaces.presentation.feign.QueueClient;
 import com.example.orderapi.interfaces.presentation.request.OrderRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Slf4j
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
@@ -17,15 +24,21 @@ public class OrderController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/orders")
-    public void createOrder(@RequestBody OrderRequest request) {
-        validateQueueProceed();
-        orderService.createOrder(request.itemId(), request.quantity());
-    }
-
-    private void validateQueueProceed() {
+    public ResponseEntity createOrder(@RequestBody OrderRequest request) {
         boolean result = queueClient.getBy("order");
         if (!result) {
-            throw new IllegalArgumentException("토큰 확인 필요");
+            try {
+                URI redirectUri = new URI("http://localhost:8083/api/v1/queue");
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setLocation(redirectUri);
+                return ResponseEntity.status(HttpStatus.SEE_OTHER).headers(httpHeaders).build();
+            } catch (URISyntaxException e) {
+                log.error("url 변환 오류");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
         }
+
+        orderService.createOrder(request.itemId(), request.quantity());
+        return ResponseEntity.ok().build();
     }
 }
