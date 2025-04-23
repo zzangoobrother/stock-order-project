3463a346625760ca8b802cb7ba9517c38c4e12ea
choiseonkang <com1368@naver.com>
2025-04-23 20:27:36 +0900

    feat : QueueClient exception 재처리 추가 구현

--- item-api/src/main/java/com/example/itemapi/interfaces/presentation/feign/QueueClient.java
+++ item-api/src/main/java/com/example/itemapi/interfaces/presentation/feign/QueueClient.java
@@ -1,10 +1,11 @@
 package com.example.itemapi.interfaces.presentation.feign;
 
+import com.example.itemapi.global.exception.QueueFeignClientErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "queueClient", url = "http://localhost:8083/api/v1/queue")
+@FeignClient(value = "queueClient", url = "http://localhost:8083/api/v1/queue", configuration = QueueFeignClientErrorDecoder.class)
 public interface QueueClient {
 
     @GetMapping(value = "/allowed", consumes = "application/json; charset=UTF-8")
