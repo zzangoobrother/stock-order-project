6c350e74ef9c1129146fa74ae7f764ebd9f9b8b0
Choi Seon Kang <42162127+zzangoobrother@users.noreply.github.com>
2025-05-14 17:30:52 +0900

    feat : item 등록 docs 구현

---
+++ item-api/src/test/java/com/example/itemapi/documentation/Documentation.java
@@ -0,0 +1,35 @@
+package com.example.itemapi.documentation;
+
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith({RestDocumentationExtension.class})
+public class Documentation {
+
+	@LocalServerPort
+	int port;
+
+	protected RequestSpecification spec;
+
+	@BeforeEach
+	public void setUp(RestDocumentationContextProvider restDocumentation) {
+		RestAssured.port = port;
+
+		this.spec = new RequestSpecBuilder()
+			.addFilter(documentationConfiguration(restDocumentation))
+			.build();
+	}
+}
---
+++ item-api/src/test/java/com/example/itemapi/documentation/ItemDocumentation.java
@@ -0,0 +1,51 @@
+package com.example.itemapi.documentation;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.*;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.example.itemapi.application.service.ItemService;
+import com.example.itemapi.interfaces.presentation.feign.QueueClient;
+import com.example.itemapi.interfaces.presentation.request.ItemRequest;
+
+import io.restassured.RestAssured;
+
+public class ItemDocumentation extends Documentation {
+
+	@MockitoBean
+	private ItemService itemService;
+
+	@MockitoBean
+	private QueueClient queueClient;
+
+	@Test
+	void addItem() {
+		ItemRequest.AddItem request = ItemRequest.AddItem.builder()
+			.name("모자")
+			.price(BigDecimal.ZERO)
+			.stock(10)
+			.build();
+
+		RestAssured
+			.given(spec).log().all()
+			.filter(document("/items",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("name").description("제품명"),
+					fieldWithPath("price").description("가격"),
+					fieldWithPath("stock").description("수량")
+				)))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(request)
+			.when().post("/api/v1/items")
+			.then().log().all().extract();
+	}
+}
